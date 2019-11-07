36756669b143906461804fd40b006780d5026588
agathadantas <agathadantasmagro@hotmail.com>
2019-11-06 21:56:20 -0300

    Usando post dentro da API

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,18 +1,30 @@
-const contatos = require("../model/contatos");
+const model = require("../model/contatos");
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.model);
+  response.status(200).send(model.agenda);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
-};
+
+
+const getAdd = (request, response) => {
+  let nomeContato = request.body
+  
+  nomeContato.id = Math.random().toString(36).substr(-8)
+
+  model.agenda.contatos.push(nomeContato)
+  response.status(200).send()
+
+}
+
+
+
+
+
 
 
 module.exports = {
   getAll,
-  getById
+  getAdd
 }
 
